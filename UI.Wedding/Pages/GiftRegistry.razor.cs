86d4f0ef38330403454e44f966247adfdc6dd70a
De Villiers du Plessis <devilliers.duplessis@gmail.com>
2022-10-05 17:21:53 +0200

    ini

---
+++ UI.Wedding/Pages/GiftRegistry.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Wedding.Pages
+{
+    public class GiftRegistryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
